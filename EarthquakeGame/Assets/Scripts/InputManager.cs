a5e4fa191e9c0d0d1f0763d77dd7f5daf192991f
paoloyanes <paoloyanes01@gmail.com>
2022-09-13 21:14:50 +0800

    Added Jump and Look Function

--- EarthquakeGame/Assets/Scripts/InputManager.cs
+++ EarthquakeGame/Assets/Scripts/InputManager.cs
@@ -7,14 +7,20 @@ public class InputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
+
     private PlayerMotor motor;
+    private PlayerLook look;
 
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+
         motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+        onFoot.Jump.performed += ctx => motor.Jump();
     }
 
     // Update is called once per frame
@@ -22,6 +28,11 @@ public class InputManager : MonoBehaviour
     {
         // tell the playermotor to move using the value from our movement action :)
         motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
--- EarthquakeGame/Assets/Scripts/PlayerMotor.cs
+++ EarthquakeGame/Assets/Scripts/PlayerMotor.cs
@@ -6,7 +6,10 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private bool isGrounded;
     public float speed = 5f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
 
     // receive the inputs for our InputManager.cs  and apply them to our character controller :)
@@ -27,5 +30,21 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -0.5f * gravity);
+        }
     }
 }
